e3b464942d7bca077dfd1966ed0be513a754a006
Devon Chou <devonchoucs@gmail.com>
2023-10-08 14:36:33 +0800

    feat(server): enhance main server setup with Swagger, routes, and global error handler

--- express-api/src/index.js
+++ express-api/src/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const { join } = require('path');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const { HOST, PORT } = require('./config');
+require('./models/associations'); // Setup model associations
+const usersRouter = require('./routes/users');
+const cppQuizzesRouter = require('./routes/cpp-quizzes');
+const logger = require('./utils/logger');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Parse incoming JSON payloads
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+// Load and setup Swagger documentation
+const swaggerDocument = YAML.load(join(__dirname, './docs/swagger/bundled.yaml'));
+app.use('/express-api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Attach routers
+app.use('/express-api/users', usersRouter);
+app.use('/express-api/cpp-quizzes', cppQuizzesRouter);
+
+// Global error handler middleware
+/* eslint-disable-next-line no-unused-vars */
+app.use((err, req, res, next) => {
+  logger.error(`Unhandled Error: ${err.message || err.toString()}`);
+  res.status(500).send('Server Error');
+});
+
+// Start the Express server
+app.listen(PORT, HOST, () => {
+  logger.info(`Server is up on ${HOST}:${PORT}`);
 });
