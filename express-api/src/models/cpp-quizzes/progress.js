d7d3048295fcd43368b7be2785abdec377b85458
Devon Chou <devonchoucs@gmail.com>
2023-10-08 13:15:45 +0800

    feat(models): add CppQuizProgress model for tracking user's quiz progress

---
+++ express-api/src/models/cpp-quizzes/progress.js
@@ -0,0 +1,62 @@
+const { DataTypes, Model } = require('sequelize');
+
+const { sequelize } = require('../../db/sequelize');
+
+// Sequelize model for tracking a user's progress in the C++ quiz
+class CppQuizProgress extends Model {}
+
+CppQuizProgress.init(
+  {
+    // User reference
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      field: 'user_id',
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    // Current round number
+    currentRound: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      field: 'current_round',
+    },
+    // Current question or stage within the round
+    currentQuestion: {
+      type: DataTypes.ENUM(
+        'Q1',
+        'Q1_FEEDBACK',
+        'Q1_DISCUSSION',
+        'Q2',
+        'Q3',
+      ),
+      allowNull: false,
+      defaultValue: 'Q1',
+      field: 'current_question',
+    },
+    // Group categorization for the user
+    group: {
+      type: DataTypes.ENUM(
+        'NOT_SPECIFIED',
+        'CONTROL',
+        'NON_ADAPTIVE',
+        'ADAPTIVE',
+        'EXCLUDED',
+      ),
+      allowNull: false,
+      defaultValue: 'NOT_SPECIFIED',
+    },
+  },
+  {
+    sequelize,
+    modelName: 'CppQuizProgress',
+    tableName: 'cpp_quiz_progresses',
+    underscored: true,
+  },
+);
+
+module.exports = CppQuizProgress;
