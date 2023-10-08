a2770c4ea92c1a0189f5ae70929acbe04ee6aa67
Devon Chou <devonchoucs@gmail.com>
2023-10-08 13:32:40 +0800

    feat(scripts): add script to create an admin user

---
+++ express-api/src/scripts/create-admin.js
@@ -0,0 +1,33 @@
+const { sequelize, connectToDatabase } = require('../db/sequelize');
+const User = require('../models/users/user');
+require('../models/associations'); // Importing model associations
+const { ADMIN_USERNAME, ADMIN_PASSWORD } = require('../config');
+const logger = require('../utils/logger');
+
+async function createAdmin() {
+  try {
+    // Connect to the database
+    await connectToDatabase();
+
+    // Create a new User instance for the admin
+    const admin = new User({
+      username: ADMIN_USERNAME,
+      password: ADMIN_PASSWORD,
+      isAdmin: true,
+    });
+
+    // Save the admin user to the database
+    await admin.save();
+
+    logger.info('Admin user created successfully!');
+
+    // Close the database connection
+    await sequelize.close();
+  } catch (e) {
+    logger.error(`Error during create admin: ${e.message || e.toString()}`);
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+// Run the function to create an admin user
+createAdmin();
