f765f9de2f63ee49d1fcda9239d519e3b9a3ba79
Devon Chou <devonchoucs@gmail.com>
2023-10-08 13:23:48 +0800

    feat(config): add unified configuration extraction from environment variables

---
+++ express-api/src/config.js
@@ -0,0 +1,36 @@
+// Extract environment variables
+const {
+  DB_HOST,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  JWT_SECRET,
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD,
+  HOST = '0.0.0.0', // Default to '0.0.0.0' if not provided
+  PORT = 3000, // Default to 3000 if not provided
+} = process.env;
+
+// Check if all required environment variables are set
+if (
+  !DB_HOST
+  || !DB_NAME
+  || !DB_USER
+  || !DB_PASSWORD
+  || !JWT_SECRET
+  || !ADMIN_USERNAME
+  || !ADMIN_PASSWORD
+) throw new Error('Missing essential environment variables.');
+
+// Export the extracted environment variables for use in the application
+module.exports = {
+  DB_HOST,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  JWT_SECRET,
+  ADMIN_USERNAME,
+  ADMIN_PASSWORD,
+  HOST,
+  PORT,
+};
