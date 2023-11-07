327052fc5290dd06de9ea223e09a5ea5596e7d41
Devon Chou <devonchoucs@gmail.com>
2023-11-07 12:37:06 +0800

    feat(session): add session middleware for user session handling

--- express-api/src/index.js
+++ express-api/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const session = require('express-session');
 const { join } = require('path');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
-const { HOST, PORT } = require('./config');
+const { HOST, PORT, SESSION_SECRET } = require('./config');
 require('./models/associations'); // Setup model associations
 const usersRouter = require('./routes/users');
 const cppQuizzesRouter = require('./routes/cpp-quizzes');
@@ -12,6 +13,17 @@ const logger = require('./utils/logger');
 const app = express();
 
 app.use(express.json()); // Parse incoming JSON payloads
+
+// session middleware
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
 
 // Load and setup Swagger documentation
 const swaggerDocument = YAML.load(join(__dirname, './docs/swagger/bundled.yaml'));
