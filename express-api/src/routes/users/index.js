100749346454aee84e1f4e0448151780041762b3
Devon Chou <devonchoucs@gmail.com>
2023-11-08 10:24:08 +0800

    refactor(routes): integrate user routes into single index.js
    - Combine individual user-related route files into a single index.js to simplify route management.

--- express-api/src/routes/users/index.js
+++ express-api/src/routes/users/index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 
-// Importing individual user-related routers
-const createRouter = require('./create');
-const loginRouter = require('./login');
-const logoutRouter = require('./logout');
-const changePasswordRouter = require('./change-password');
+const auth = require('../../middleware/auth');
+const checkAdmin = require('../../middleware/check-admin');
+const {
+  createUser,
+  login,
+  logout,
+  changePassword,
+} = require('../../controllers/users/account');
 
 const router = express.Router();
 
-// Mounting the routers
-router.use('/', createRouter); // Router for user creation
-router.use('/login', loginRouter); // Router for user login
-router.use('/logout', logoutRouter); // Router for user logout
-router.use('/me/password', changePasswordRouter); // Router to change password for the authenticated user
+// Route to create a user, requires authentication and admin privileges
+router.post('/', auth, checkAdmin, createUser);
+
+// Route to handle user login
+router.post('/login', login);
+
+// Route to handle user logout, requires authentication
+router.post('/logout', auth, logout);
+
+// Route to handle password change, requires authentication
+router.patch('/me/password', auth, changePassword);
 
 module.exports = router;
