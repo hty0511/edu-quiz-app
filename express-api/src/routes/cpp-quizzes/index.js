1119f5ad0490ebf75984aa95c0712fe9e02288ac
Devon Chou <devonchoucs@gmail.com>
2023-11-08 10:59:39 +0800

    feat(routes): add reset all C++ quiz progress route
    - Introduce a new POST route '/progresses/reset-all' to allow admin users to reset all C++ quiz progress records.

--- express-api/src/routes/cpp-quizzes/index.js
+++ express-api/src/routes/cpp-quizzes/index.js
@@ -4,11 +4,13 @@ const auth = require('../../middleware/auth');
 const checkAdmin = require('../../middleware/check-admin');
 const {
   createCppQuizProgress,
+  resetAllCppQuizProgress,
 } = require('../../controllers/cpp-quizzes/progress');
 
 const router = express.Router();
 
 // Progress routes
 router.post('/progresses', auth, checkAdmin, createCppQuizProgress);
+router.post('/progresses/reset-all', auth, checkAdmin, resetAllCppQuizProgress);
 
 module.exports = router;
