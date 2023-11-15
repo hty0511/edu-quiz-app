d003b477e6cbe26bddacea569d2c7da4b2fe6dfd
Devon Chou <devonchoucs@gmail.com>
2023-11-15 15:26:56 +0800

    fix: add missing next parameter in errorHandler to comply with Express standards
    - Include `next` parameter in errorHandler to align with Express error handling conventions.
    - Disable ESLint's no-unused-vars rule in errorHandler to avoid warnings for unused next parameter.

--- express-api/src/middleware/error-handler.js
+++ express-api/src/middleware/error-handler.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
+
 const logger = require('../utils/logger');
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   logger.error(`Error: ${err.message || err.toString()}`);
   res
     .status(err.statusCode || 500)
