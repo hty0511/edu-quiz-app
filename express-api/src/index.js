5ce6a23ac81a0c8e1c08a971e34d2b11bbe92ced
Devon Chou <devonchoucs@gmail.com>
2023-11-23 09:20:28 +0800

    refactor: restructure entry point into app.js and index.js
    - Separated the application's setup and server starting logic.
    - Created app.js for initializing Express app and middlewares.
    - Updated index.js to focus on starting the server.

--- express-api/src/index.js
+++ express-api/src/index.js
@@ -1,41 +1,6 @@
-const express = require('express');
-const session = require('express-session');
-const { join } = require('path');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-
-const { HOST, PORT, SESSION_SECRET } = require('./config');
-require('./models/associations'); // Setup model associations
-const usersRouter = require('./routes/users');
-const cppQuizzesRouter = require('./routes/cpp-quizzes');
-const errorHandler = require('./middleware/error-handler');
+const app = require('./app');
+const { HOST, PORT } = require('./config');
 const logger = require('./utils/logger');
-
-const app = express();
-
-app.use(express.json()); // Parse incoming JSON payloads
-
-// session middleware
-app.use(session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
-
-// Load and setup Swagger documentation
-const swaggerDocument = YAML.load(join(__dirname, './docs/swagger/bundled.yaml'));
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Attach routers
-app.use('/api/users', usersRouter);
-app.use('/api/cpp-quizzes', cppQuizzesRouter);
-
-// Global error handler middleware
-app.use(errorHandler);
 
 // Start the Express server
 app.listen(PORT, HOST, () => {
