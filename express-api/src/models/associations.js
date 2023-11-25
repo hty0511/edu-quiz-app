31bc8add7d895fae523293d740b152ab8227fcf6
Devon Chou <devonchoucs@gmail.com>
2023-11-25 11:29:34 +0800

    chore(models): updated models associations

--- express-api/src/models/associations.js
+++ express-api/src/models/associations.js
@@ -9,23 +9,49 @@ const UserAnswerQ2 = require('./cpp-quizzes/answers/q2');
 const UserAnswerQ3 = require('./cpp-quizzes/answers/q3');
 
 // User associations
-User.hasOne(CppQuizProgress, { foreignKey: 'userId', as: 'cppQuizProgress' });
-User.hasMany(UserAnswerQ1, { foreignKey: 'userId', as: 'q1-answers' });
-User.hasMany(UserAnswerQ1Feedback, { foreignKey: 'userId', as: 'q1-feedback-answers' });
-User.hasMany(UserAnswerQ1Discussion, { foreignKey: 'userId', as: 'q1-discussion-answers' });
-User.hasMany(UserAnswerQ2, { foreignKey: 'userId', as: 'q2-answers' });
-User.hasMany(UserAnswerQ3, { foreignKey: 'userId', as: 'q3-answers' });
-
-CppQuizProgress.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-UserAnswerQ1.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-UserAnswerQ1Feedback.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-UserAnswerQ1Discussion.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-UserAnswerQ2.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-UserAnswerQ3.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+User.hasOne(CppQuizProgress, {
+  foreignKey: { name: 'userId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+User.hasMany(UserAnswerQ1, {
+  foreignKey: { name: 'userId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+User.hasMany(UserAnswerQ1Feedback, {
+  foreignKey: { name: 'userId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+User.hasMany(UserAnswerQ1Discussion, {
+  foreignKey: { name: 'userId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+User.hasMany(UserAnswerQ2, {
+  foreignKey: { name: 'userId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+User.hasMany(UserAnswerQ3, {
+  foreignKey: { name: 'userId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
 
 // Question associations
-UserAnswerQ1.belongsTo(Question, { foreignKey: 'questionId', as: 'question' });
-UserAnswerQ1Feedback.belongsTo(Question, { foreignKey: 'questionId', as: 'question' });
-UserAnswerQ1Discussion.belongsTo(Question, { foreignKey: 'questionId', as: 'question' });
-UserAnswerQ2.belongsTo(Question, { foreignKey: 'questionId', as: 'question' });
-UserAnswerQ3.belongsTo(Question, { foreignKey: 'questionId', as: 'question' });
+Question.hasMany(UserAnswerQ1, {
+  foreignKey: { name: 'questionId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+Question.hasMany(UserAnswerQ1Feedback, {
+  foreignKey: { name: 'questionId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+Question.hasMany(UserAnswerQ1Discussion, {
+  foreignKey: { name: 'questionId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+Question.hasMany(UserAnswerQ2, {
+  foreignKey: { name: 'questionId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
+Question.hasMany(UserAnswerQ3, {
+  foreignKey: { name: 'questionId', allowNull: false },
+  onDelete: 'RESTRICT',
+});
