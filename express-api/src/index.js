8307a15269d4246493fc6ac95668616035f79431
Devon Chou <devonchoucs@gmail.com>
2023-11-25 11:45:04 +0800

    feat(server): Add graceful database disconnect on application shutdown

--- express-api/src/index.js
+++ express-api/src/index.js
@@ -1,8 +1,21 @@
 const app = require('./app');
 const { HOST, PORT } = require('./config');
+const { sequelize } = require('./db/sequelize');
 const logger = require('./utils/logger');
 
 // Start the Express server
 app.listen(PORT, HOST, () => {
   logger.info(`Server is up on ${HOST}:${PORT}`);
+});
+
+process.on('SIGTERM', async () => {
+  logger.info('SIGTERM signal received: closing database connection...');
+  try {
+    await sequelize.close();
+    logger.info('Database connection closed.');
+    setTimeout(() => process.exit(0), 1000);
+  } catch (error) {
+    logger.error(`Failed to close database connection: ${error.message || error.toString()}`);
+    setTimeout(() => process.exit(1), 1000);
+  }
 });
