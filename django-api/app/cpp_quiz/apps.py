9683067058e8b3ef57981aa570379fb868789235
Devon Chou <devonchoucs@gmail.com>
2023-11-29 10:37:40 +0800

    feat(django-api): add pre-load sentence-transformers model funtionality

--- django-api/app/cpp_quiz/apps.py
+++ django-api/app/cpp_quiz/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import os
+from sentence_transformers import SentenceTransformer
 
 
 class CppQuizConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cpp_quiz'
+
+    def ready(self):
+        # 加载模型的代码
+        model_path = os.path.join(os.path.dirname(__file__), 'my_model')
+        self.model = SentenceTransformer(model_path)
--- django-api/app/cpp_quiz/views.py
+++ django-api/app/cpp_quiz/views.py
@@ -1,10 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.apps import apps
+from sentence_transformers import util
 from django.http import JsonResponse
+
+
+cpp_quiz_config = apps.get_app_config('cpp_quiz')
+
 
 class SystemFeedbackView(APIView):
     def post(self, request, *args, **kwargs):
-        # 处理 POST 请求
-        received_data = request.data
-        # 这里可以根据需要处理 received_data
-        return JsonResponse({"systemFeedback": "feedback from django api"})
+        model = cpp_quiz_config.model
+
+        s1 = '在這個風和日麗的早晨，我們決定一同前往郊外的湖邊，那裡的風景如畫，湖水清澈見底，周圍的綠意盎然和花香四溢，讓人感到放鬆和愉悅。'
+        s2 = '這個晴朗的下午，一群朋友興致勃勃地計劃去郊外踏青，目的地是一個風景宜人的小湖，它周圍環繞著茂密的樹木和五彩斑斕的野花，為人們提供了一個完美的逃離城市喧囂的地方。'
+        embeddings = model.encode([s1, s2], convert_to_tensor=True)
+
+        cosine_scores = util.pytorch_cos_sim(embeddings[0], embeddings[1])
+
+        return JsonResponse({"systemFeedback": "feedback from django api", "sim": cosine_scores.item()})
